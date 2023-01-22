daec16ed539ead43366026e286a99de8d032c31e
Lucas <96322105+luca4s@users.noreply.github.com>
2023-01-22 17:45:42 -0300

    static toggle

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var x = setInterval(function() {
     var distance = finalbattle - now;
     var time = [Math.floor(distance / (1000 * 60 * 60 * 24)), Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)), Math.floor((distance % (1000 * 60)) / 1000)];
     for (let i = 0; i < time.length; i++) {
-        time[i] = `${time[i]}`
+        time[i] = `${time[i]}`;
         if (time[i].length < 2) {
-            time[i] = `0${time[i]}`
-        }
-    }
-    document.getElementById("time").innerHTML = `${time[0]}:${time[1]}:${time[2]}:${time[3]}`
+            time[i] = `0${time[i]}`;
+        };
+    };
+    document.getElementById("time").innerHTML = `${time[0]}:${time[1]}:${time[2]}:${time[3]}`;
     if (distance < 1000) {
-        document.getElementById("time").innerHTML = "00:00:00:00"
+        document.getElementById("time").innerHTML = "00:00:00:00";
         clearInterval(x);
     };
 }, 1);
